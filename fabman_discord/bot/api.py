77ad964dd25cd0bbf5c3d28bcef7de83b525a123
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2024-08-05 15:05:34 +0200

    channel_id should be string

--- fabman_discord/bot/api.py
+++ fabman_discord/bot/api.py
@@ -5,7 +5,7 @@ from fabman_discord.dependencies import get_settings
 settings = get_settings()
 
 
-def send_message(channel_id: int, message: str):
+def send_message(channel_id: str, message: str):
     url = f"https://discord.com/api/v10/channels/{channel_id}/messages"
     authorization = f"Bot {settings.discord.bot_token}"
     requests.post(
