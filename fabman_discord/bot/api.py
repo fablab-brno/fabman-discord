86f2c4869fb18eb7c7722e50918cf43852b3d2e6
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2024-12-04 09:40:48 +0100

    extend logging of successful messages

--- fabman_discord/bot/api.py
+++ fabman_discord/bot/api.py
@@ -8,11 +8,17 @@ settings = get_settings()
 def send_message(channel_id: str, message: str):
     url = f"https://discord.com/api/v10/channels/{channel_id}/messages"
     authorization = f"Bot {settings.discord.bot_token}"
-    response = requests.post(
-        url, headers={"Authorization": authorization}, json={"content": message}
-    )
+    try:
+        response = requests.post(
+            url, headers={"Authorization": authorization}, json={"content": message}
+        )
+    except Exception as e:
+        print(f"Unknown error: {e}")
+        return
 
     if response.status_code != 200:
         print(
-            f"Message failed to send to {channel_id}: {response.status_code} {response.content}"
+            f"Message failed to send to {channel_id}: {response.status_code} {response.text}"
         )
+    else:
+        print(response.text)
