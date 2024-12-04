6c9c2fb681411558856a63899f1d61bae2b96c57
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2024-12-04 09:35:22 +0100

    add simple logging

--- fabman_discord/bot/api.py
+++ fabman_discord/bot/api.py
@@ -8,6 +8,11 @@ settings = get_settings()
 def send_message(channel_id: str, message: str):
     url = f"https://discord.com/api/v10/channels/{channel_id}/messages"
     authorization = f"Bot {settings.discord.bot_token}"
-    requests.post(
+    response = requests.post(
         url, headers={"Authorization": authorization}, json={"content": message}
     )
+
+    if response.status_code != 200:
+        print(
+            f"Message failed to send to {channel_id}: {response.status_code} {response.content}"
+        )
